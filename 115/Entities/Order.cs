5e086b3458c91844c6efa54095553c3ae4d88ba0
Augusto Ribeiro <augustoribeiro86@hotmail.com>
2021-09-10 14:25:00 -0300

    Primeiro projeto Enums

---
+++ 115/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using _115.Entities.Enums;
+
+
+namespace _115.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Moment}, {Status}";
+        }
+    }
+}
---
+++ 115/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using _115.Entities;
+using _115.Entities.Enums;
+
+namespace _115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.Processing.ToString();
+            OrderStatus txt2 = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Console.WriteLine(txt);
+            Console.WriteLine(txt2);
+
+        }
+    }
+}
